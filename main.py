6ccd6bf79fcfe296b20739dd6d214e3d60d7272d
farhan <nahidahmed9933@gmail.com>
2023-03-10 23:48:25 +0600

    validation added using pydantic

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
 from fastapi import FastAPI
 from fastapi.params import Body
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    ratting: Optional[int]
+    published: bool = True
 
 app = FastAPI()
 
@@ -8,9 +19,13 @@ async def root():
     return {"message" : "Hello world!"}
 
 
+# @app.post('/create-post')
+# async def create_post(payload: dict = Body(...)):
+#     return payload
+
 @app.post('/create-post')
-async def create_post(payload: dict = Body(...)):
-    return payload
+async def create_post(post:Post):
+    return post.dict()
 
 @app.get("/posts")
 async def get_post():
